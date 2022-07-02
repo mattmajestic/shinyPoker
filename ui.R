7649495b2be69db985957474186ca6b137a7ee4b
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-07-02 10:12:56 -0400

    minor fix

--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ ui <- navbarPage(title = "shinyPoker",windowTitle = "shinyPoker",
   br(),
   column(4,
   numericInput("wsop_id","WSOP ID",value = ids[1]),
-  selectInput("wsop_name","WSOP Player",value = names[1])
+  selectInput("wsop_name","WSOP Player",choices = names[1])
   ),
   column(8,
   DTOutput("wsop_dt")
